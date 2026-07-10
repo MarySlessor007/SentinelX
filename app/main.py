d8a908d203ed16f792edec4f0f95c73107713910
MARY SLESSOR <maryslessor@Mac.lan>
2026-07-10 17:43:09 -0500

    Add SSH brute force detection engine

--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
 from parser.log_parser import LogParser
+from engine.detection_engine import DetectionEngine
 
 
 def main():
-    parser = LogParser("sample_logs/auth.log")
+
+    parser = LogParser(
+        "sample_logs/auth.log"
+    )
 
     logs = parser.parse_logs()
 
-    print("\nParsed Log Entries\n")
 
-    for log in logs:
-        print(log)
+    engine = DetectionEngine()
+
+
+    alerts = engine.detect_brute_force(logs)
+
+
+    print("\nSecurity Alerts\n")
+
+
+    for alert in alerts:
+        print(alert)
+
 
 
 if __name__ == "__main__":
---
+++ engine/detection_engine.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+
+class DetectionEngine:
+
+    def __init__(self, threshold=5):
+        self.threshold = threshold
+
+
+    def detect_brute_force(self, logs):
+
+        failed_attempts = defaultdict(int)
+
+        alerts = []
+
+
+        for log in logs:
+
+            if log["status"] == "Failed":
+                failed_attempts[log["ip"]] += 1
+
+
+        for ip, count in failed_attempts.items():
+
+            if count >= self.threshold:
+
+                alerts.append(
+                    {
+                        "type": "SSH Brute Force",
+                        "source_ip": ip,
+                        "attempts": count,
+                        "severity": "HIGH"
+                    }
+                )
+
+        return alerts
